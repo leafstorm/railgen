0fae4216fcd39444292eabc13e050d09930ef7c9
Thomas Daede <daede003@umn.edu>
2012-01-05 21:05:20 -0600

    Add proper loop rail traversal and fix twoway rails.

--- generate-nodes.py
+++ generate-nodes.py
@@ -14,10 +14,12 @@ parser = argparse.ArgumentParser(description='''
 Generates a json file containing node link information,
 to be used to determine fastest route.''')
 parser.add_argument('inputfile', metavar='INPUT', type=str, help='input yaml file')
+parser.add_argument('outputfile', metavar='OUTPUT', type=str, help='output json file')
 args = parser.parse_args()
 
 yaml_file = open(args.inputfile, 'r')
 yaml_data = yaml.load(yaml_file)
+yaml_file.close()
 
 stations = yaml_data["stations"]
 lines = yaml_data["lines"]
@@ -26,16 +28,21 @@ for station in stations:
 	stations[station]["destinations"] = []
 
 for linename,line in lines.iteritems():
-	if line["flow"] == "twoway":
-		previous = ""
-		for stopindex, stop in enumerate(line["stops"]):
-			try:
-				stations[getStopStation(stop)]["destinations"].append(getStopStation(line["stops"][stopindex-1]))
-			except IndexError:
-				pass
-			try:
-				stations[getStopStation(stop)]["destinations"].append(getStopStation(line["stops"][stopindex+1]))
-			except IndexError:
-				pass
+	previous = ""
+	for stopindex, stop in enumerate(line["stops"]):
+		if stopindex > 0 or line["flow"] == "loop":
+			previousStation = getStopStation(line["stops"][stopindex-1])
+			stations[getStopStation(stop)]["destinations"].append(previousStation)
+		try:
+			if line["flow"] == "loop" and stopindex+1 > len(line["stops"])-1:
+				nextStation = getStopStation(line["stops"][0])
+			else:
+				nextStation = getStopStation(line["stops"][stopindex+1])
+			stations[getStopStation(stop)]["destinations"].append(nextStation)
+		except IndexError:
+			pass
 			
 pprint(stations)
+json_file = open(args.outputfile, 'w')
+json.dump(stations, json_file)
+json_file.close()
